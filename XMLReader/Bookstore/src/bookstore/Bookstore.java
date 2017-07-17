ee77f55a8521c1a50f3adeff4821412447ef20b4
josephinex <josephinex21@gmail.com>
2017-07-17 14:41:28 +0300

    Add serializable keyword. Add XML type

--- XMLReader/Bookstore/src/bookstore/Bookstore.java
+++ XMLReader/Bookstore/src/bookstore/Bookstore.java
@@ -1,13 +1,21 @@
+package bookstore;
+import java.io.Serializable;
 import java.util.List;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 
-@XmlRootElement
+@XmlRootElement(namespace="", name="bookstore")
 @XmlAccessorType(XmlAccessType.FIELD)
-public class Bookstore {
+@XmlType(name = "bookstore", propOrder = {
+	    "book"
+	})
+public class Bookstore implements Serializable{
+	
+	private static final long serialVersionUID = -7844966642164040872L;
 	
 	@XmlElement 
 	List<Book> book;
