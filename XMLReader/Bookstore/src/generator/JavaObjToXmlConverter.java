9d73b4cbdc53ce0ff3c1193a309a623a130a3d4a
josephinex <josephinex21@gmail.com>
2017-07-22 15:46:31 +0300

    Add XPath expressions examples. Add new element

--- XMLReader/Bookstore/src/generator/JavaObjToXmlConverter.java
+++ XMLReader/Bookstore/src/generator/JavaObjToXmlConverter.java
@@ -22,6 +22,7 @@ public class JavaObjToXmlConverter {
 		book.setCost(100);
 		book.setPublisher("Publisher");
 		book.setISBN(98653323232L);
+		book.setId("harry_potter");
 		
 		Book book2 = new Book();
 		book2.setAuthor("J. R. R. Tolkien");
@@ -30,6 +31,7 @@ public class JavaObjToXmlConverter {
 		book2.setCost(100);
 		book2.setPublisher("Publisher");
 		book2.setISBN(98665323232L);
+		book2.setId("hobbit");
 		
 		Book book3 = new Book();
 		book3.setAuthor("Oscar Wilde");
@@ -38,6 +40,7 @@ public class JavaObjToXmlConverter {
 		book3.setCost(150);
 		book3.setPublisher("Publisher");
 		book3.setISBN(98765677232L);
+		book3.setId("dorian_gray");
 		
 		Book book4 = new Book();
 		book4.setAuthor("Jane Austen");
@@ -46,6 +49,7 @@ public class JavaObjToXmlConverter {
 		book4.setCost(180);
 		book4.setPublisher("Publisher");
 		book4.setISBN(98765677232L);
+		book4.setId("pride_prejudice");
 		
 		Book book5 = new Book();
 		book5.setAuthor("Victor Hugo");
@@ -54,6 +58,16 @@ public class JavaObjToXmlConverter {
 		book5.setCost(190);
 		book5.setPublisher("Publisher");
 		book5.setISBN(98765677232L);
+		book5.setId("homme_qui_rit");
+		
+		Book book6 = new Book();
+		book6.setAuthor("J. R. R. Tolkien");
+		book6.setDate(new Date());
+		book6.setTitle("The Lord of the Rings");
+		book6.setCost(100);
+		book6.setPublisher("Publisher");
+		book6.setISBN(98665323232L);
+		book6.setId("lord_rings");
 		
 		
 		Bookstore bookstore = new Bookstore();
@@ -63,6 +77,7 @@ public class JavaObjToXmlConverter {
 		list.add(book3);
 		list.add(book4);
 		list.add(book5);
+		list.add(book6);
 		bookstore.setBook(list);
 		
 		JAXBContext jaxbContext = null;
@@ -74,7 +89,6 @@ public class JavaObjToXmlConverter {
 			jaxbMarshaller.marshal(bookstore, new File("src/schema/bookstore.xml"));
 			jaxbMarshaller.marshal(bookstore, System.out);
 		} catch (JAXBException e) {
-			
 			e.printStackTrace();
 		}
 		
--- XMLReader/Bookstore/src/generator/XmlToJavaObjectXpathConverter.java
+++ XMLReader/Bookstore/src/generator/XmlToJavaObjectXpathConverter.java
@@ -19,20 +19,61 @@ public class XmlToJavaObjectXpathConverter {
 
 	public void convertToJavaObject()
 			throws ParserConfigurationException, SAXException, IOException, XPathExpressionException {
-		
+
 		DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
 		domFactory.setNamespaceAware(true);
 		DocumentBuilder builder = domFactory.newDocumentBuilder();
 		Document doc = builder.parse("src/schema/bookstore.xml");
-		
+
 		XPath xpath = XPathFactory.newInstance().newXPath();
 		XPathExpression expr = xpath.compile("//book/*/text()");
-		
+
 		Object result = expr.evaluate(doc, XPathConstants.NODESET);
 		NodeList nodes = (NodeList) result;
-		
-		for (int i = 0; i < nodes.getLength(); i++) {
-			System.out.println(nodes.item(i).getNodeValue());
+
+		/*
+		 * for (int i = 0; i < nodes.getLength(); i++) {
+		 * System.out.println(nodes.item(i).getNodeValue()); }
+		 */
+
+		// return all book authors
+		XPathExpression expr2 = xpath.compile("//book/author/text()");
+		Object result2 = expr2.evaluate(doc, XPathConstants.NODESET);
+		NodeList nodes2 = (NodeList) result2;
+
+		for (int i = 0; i < nodes2.getLength(); i++) {
+			System.out.println(nodes2.item(i).getNodeValue());
+		}
+
+		// return all books by a particular author
+
+		XPathExpression expr3 = xpath.compile("//book[contains(author, 'J. R. R. Tolkien')]/title/text()");
+		Object result3 = expr3.evaluate(doc, XPathConstants.NODESET);
+		NodeList nodes3 = (NodeList) result3;
+
+		for (int i = 0; i < nodes3.getLength(); i++) {
+			System.out.println("The books by author are: " + nodes3.item(i).getNodeValue());
+		}
+
+		// return all books cheaper than 120$
+
+		XPathExpression expr4 = xpath.compile("//book[cost < 120]/title/text()");
+		Object result4 = expr4.evaluate(doc, XPathConstants.NODESET);
+		NodeList nodes4 = (NodeList) result4;
+
+		for (int i = 0; i < nodes4.getLength(); i++) {
+			System.out.println("Books cheaper than 120$ are : " + nodes4.item(i).getNodeValue());
+		}
+
+		// return all authors of the books which are more expensive than 150$
+
+		XPathExpression expr5 = xpath.compile("//book[cost > 150]/author/text()");
+		Object result5 = expr5.evaluate(doc, XPathConstants.NODESET);
+		NodeList nodes5 = (NodeList) result5;
+
+		for (int i = 0; i < nodes5.getLength(); i++) {
+			System.out.println(
+					"Authors of the books which are more expensive than 120$: " + nodes5.item(i).getNodeValue());
 		}
 	}
 
