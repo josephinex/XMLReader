980787ae5ead004e0add0e9f382f1663f2398839
josephinex <josephinex21@gmail.com>
2017-07-22 15:45:23 +0300

    Add one more field for attribute + getter and setter

--- XMLReader/Bookstore/src/bookstore/Book.java
+++ XMLReader/Bookstore/src/bookstore/Book.java
@@ -1,37 +1,48 @@
 package bookstore;
+
 import java.io.Serializable;
 import java.util.Date;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "book", propOrder = {
-    "title",
-    "author",
-    "date",
-    "ISBN",
-    "publisher",
-    "cost"
-})
-public class Book implements Serializable{
+@XmlType(name = "book", propOrder = { "title", "author", "date", "ISBN", "publisher", "cost" })
+public class Book implements Serializable {
 
 	private static final long serialVersionUID = 2160141592416705966L;
-	
+
 	@XmlElement(required = true)
 	private String title;
+	
 	@XmlElement(required = true)
 	private String author;
+	
 	@XmlElement(required = true)
 	private Date date;
+	
 	@XmlElement(required = true)
 	private long ISBN;
+	
 	@XmlElement(required = true)
 	private String publisher;
+	
 	@XmlElement(required = true)
-	public Integer cost;
+	public int cost;
+
+	@XmlAttribute
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	public String getTitle() {
 		return title;
@@ -73,11 +84,11 @@ public class Book implements Serializable{
 		this.publisher = publisher;
 	}
 
-	public Integer getCost() {
+	public int getCost() {
 		return cost;
 	}
 
-	public void setCost(Integer cost) {
+	public void setCost(int cost) {
 		this.cost = cost;
 	}
 }
